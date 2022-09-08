cf7c7ff149be2c0210a6d80759395ab16737826d
Guido Contreras Woda <guido.woda@inviu.com.ar>
2022-09-08 13:52:04 -0300

    Second failing state: After bumping all required jackson dependencies, it seems that Akka is using an annotation that it's either missing or deprecated in jackson.

--- src/main/java/demo/DemoBehavior.java
+++ src/main/java/demo/DemoBehavior.java
@@ -1,5 +1,6 @@
 package demo;
 
+import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.persistence.typed.PersistenceId;
@@ -10,12 +11,41 @@ import demo.DemoBehavior.DemoCommand;
 import demo.DemoBehavior.DemoEvent;
 import demo.DemoBehavior.DemoState;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class DemoBehavior extends EventSourcedBehavior<DemoCommand, DemoEvent, DemoState> {
-    interface DemoCommand {}
+    interface DemoCommand extends Jsonable {}
 
-    interface DemoEvent {}
-    static class DemoState {}
+    static class Ping implements DemoCommand {
+        final ActorRef<Pong> replyTo;
+
+        Ping(ActorRef<Pong> replyTo) {
+            this.replyTo = replyTo;
+        }
+    }
+
+    static class Pong implements Jsonable {}
+
+    interface DemoEvent extends Jsonable {}
+
+    static class Pinged implements DemoEvent {
+        OffsetDateTime when;
+    }
+
+    static class DemoState implements Jsonable {
+        List<Pinged> events = new ArrayList<>();
+
+        DemoState onPinged(Pinged event) {
+            var newState = new DemoState();
+            newState.events.addAll(events);
+            newState.events.add(event);
+
+            return newState;
+        }
+    }
 
     static Behavior<DemoCommand> create() {
         return Behaviors.setup(context -> new DemoBehavior(PersistenceId.ofUniqueId("anId")));
@@ -32,11 +62,19 @@ public class DemoBehavior extends EventSourcedBehavior<DemoCommand, DemoEvent, D
 
     @Override
     public CommandHandler<DemoCommand, DemoEvent, DemoState> commandHandler() {
-        return newCommandHandlerBuilder().build();
+        return newCommandHandlerBuilder()
+                .forAnyState()
+                .onCommand(Ping.class, command -> Effect()
+                        .persist(new Pinged())
+                        .thenReply(command.replyTo, state -> new Pong()))
+                .build();
     }
 
     @Override
     public EventHandler<DemoState, DemoEvent> eventHandler() {
-        return newEventHandlerBuilder().build();
+        return newEventHandlerBuilder()
+                .forAnyState()
+                .onEvent(Pinged.class, DemoState::onPinged)
+                .build();
     }
 }
--- src/test/java/demo/ActorSerializationTest.java
+++ src/test/java/demo/ActorSerializationTest.java
@@ -2,19 +2,25 @@ package demo;
 
 import akka.actor.testkit.typed.javadsl.ActorTestKit;
 import akka.persistence.testkit.javadsl.EventSourcedBehaviorTestKit;
+import com.typesafe.config.ConfigFactory;
 import demo.DemoBehavior.DemoCommand;
 import demo.DemoBehavior.DemoEvent;
 import demo.DemoBehavior.DemoState;
+import demo.DemoBehavior.Ping;
+import demo.DemoBehavior.Pinged;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ActorSerializationTest {
-    private static final ActorTestKit testKit = ActorTestKit.create(EventSourcedBehaviorTestKit.config());
+    private static final ActorTestKit testKit = ActorTestKit.create(EventSourcedBehaviorTestKit.config()
+            .withFallback(ConfigFactory.load("application-test.conf")));
     private static final EventSourcedBehaviorTestKit<DemoCommand, DemoEvent, DemoState> persistenceTestKit = EventSourcedBehaviorTestKit.create(
             testKit.system(), DemoBehavior.create());
 
     @Test
     void asd() {
-        Assertions.assertTrue(true);
+        var result = persistenceTestKit.runCommand(Ping::new);
+        Assertions.assertNotNull(result.reply());
+        Assertions.assertNotNull(result.eventOfType(Pinged.class));
     }
 }
